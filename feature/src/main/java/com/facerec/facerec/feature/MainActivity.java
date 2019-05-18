39cc5534a4e934065d11ee588abb0519bd8c065f
jonLicari <jlicari@ryerson.ca>
2019-05-18 02:29:12 -0400

    Basic UI: welcome page with image button leading to new activity.

--- feature/src/main/java/com/facerec/facerec/feature/MainActivity.java
+++ feature/src/main/java/com/facerec/facerec/feature/MainActivity.java
@@ -1,5 +1,7 @@
 package com.facerec.facerec.feature;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +10,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton button;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +31,19 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        button = (ImageButton) findViewById(R.id.imageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamActivity();
+            }
+        });
+    }
+
+    public void openCamActivity() {
+        Intent intent = new Intent(this, CamActivity.class);
+        startActivity(intent);
     }
 
     @Override
